855aa5cfda3945f7e39eb95f83996c346955a07f
Thibault Lenclos <tlenclos@jolicode.com>
2016-01-29 13:47:26 +0100

    Add stats.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import GameState from 'states/GameState';
+import Stats from 'stats.js/src/Stats.js';
 
 class Game extends Phaser.Game {
-
 	constructor() {
 		super(500, 500, Phaser.AUTO, 'content', null);
 		this.state.add('GameState', GameState, false);
 		this.state.start('GameState');
+		this.addStats();
+	}
+
+	addStats() {
+		var stats = new Stats();
+
+		stats.setMode(0);
+
+		stats.domElement.style.position = 'absolute';
+		stats.domElement.style.right = '0px';
+		stats.domElement.style.top = '0px';
+
+		document.body.appendChild(stats.domElement);
+
+		setInterval(function () {
+			stats.begin();
+			stats.end();
+		}, 1000 / 60);
 	}
 
 }
